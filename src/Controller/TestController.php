fb302953d260d0cb27aca45122040994239ef22f
VQuemeneur <vquemeneur29@gmail.com>
2026-01-28 17:41:17 +0100

    Initial commit - Symfony project

---
+++ src/Controller/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test', methods: ['GET', 'HEAD'])]
+    public function index(Request $request): Response
+    {
+        return $this->render('test/index.html.twig');
+    }
+
+
+    #[Route('/hello/{age}/{nom}/{prenom}', name: 'hello', requirements: ["nom" => "[a-z]{2,50}"])]
+    public function hello(Request $request, int $age, $nom, $prenom = '')
+    {
+        return $this->render('test/hello.html.twig', [
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'age' => $age,
+            'messageHtml' => '<h3>je vais tester raw</h3>',
+            'monTableau' => ['profession' => 'formateur',
+                'sexe' => 'M',
+                'specialité' => 'Symfony']
+        ]);
+    }
+}
